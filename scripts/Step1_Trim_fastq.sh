ed0192ca1da3798ac3a36c2c19c7b480a8f47767
Hisashi Miura <30642262+kuzobuta@users.noreply.github.com>
2025-08-26 19:04:28 +0900

    Update Step1_Trim_fastq.sh

--- scripts/Step1_Trim_fastq.sh
+++ scripts/Step1_Trim_fastq.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-
 #scRepli-seq pipeline
 #Step1.1 Trim illumina adaptor sequence
+#Multi-thread mode
+#v250825
 
-if [ $# -le 2 ] ; then
-    echo "Usage: bash Step1_Trim_fastq.sh [index_seq] [in_fastq] [out_fastq1] [SEQXE] [out_fastq2]"
+if [ $# -le 4 ] ; then
+    echo "Usage: bash Step1_Trim_fastq.sh [index_seq] [threads] [in_fastq] [outdir] [SEQXE]"
     echo ""
     echo "Trimming the fastq file by cutadapt program using index adaptor & SEQXE primer sequence"
     echo ""
     echo "arguments:"
     echo ""
     echo "index_seq     Illumina index adaptor sequence"
+    echo "threads       CPUs for cutadapt"
     echo "in_fastq		The path of fastq file"
-    echo "out_fastq1	The path of adaptor trimmed fastq file"
+    echo "outdir    	Outdir path"
     echo "SEQXE			SEQXE adaptor sequence (optional)"
-    echo "out_fastq2	The path of adaptor & SEQXE-primer trimmed fastq file (optional)"
     echo ""
     echo "For TruSeq Single Indexes, you can use index_seq as "
     echo "GATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG"
+    echo "For SEQXE, you may use the following seq "
+    echo "TGGTGTGTTGGGTGTGTTTCTGAAGNNNNNNNNN"
     echo ""
     exit 0
 fi
@@ -28,21 +31,36 @@ echo "#scRepli-seq Step1 Adaptor trimming	start: `date`"
 index_seq=$1
 #index_seq="GATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG"
 
-in_fastq=$2
-out_fastq1=$3 #ex: QUERY.adapter_filtered.fastq.gz 
+# CPUs for cutadapt
+threads=$2
 
+in_fastq=$3
 filename=`basename $in_fastq`
 echo "#file name:${filename}"
 
+outdir=$4
+mkdir -p $outdir
 
-cutadapt -b ${index_seq} -o ${out_fastq1} ${in_fastq}
+prefix=${filename%.fastq.gz}
+out_fastq1="${outdir}/${prefix}.adapter_filtered.fastq.gz"
 
-echo "#scRepli-seq Step1 Adaptor Trimming	end: `date`"
-echo "#scRepli-seq Step1 SEQXE Trimming		start: `date`"
+if [-e "${out_fastq1}" ]; then
+
+echo "#"
+echo "# Skip adapter trimming.. Found adapter filtered fastq for ${filename}" 
+echo "#"
+
+else
+
+echo "#scRepli-seq Step1 Adaptor trimming   start: $(date)"
+cutadapt -j $threads -b ${index_seq} -o ${out_fastq1} ${in_fastq}
+echo "#scRepli-seq Step1 Adaptor Trimming	  end: $(date)"
+
+fi
 
 
 #Step1.2 Trim SEQXE primer
-SEQXE=$4
+SEQXE=$5
 out_fastq2=$5 #ex: QUERY.adapter_filtered2.fastq.gz 
 
 if [ -z "$SEQXE" ]
@@ -52,9 +70,18 @@ echo "#SEQXE Primer trimming is not performed.."
 
 else
 
+if [ -e "${out_fastq2}" ]; then
+echo "#"
+echo "# Skip SEQXE trimming.. Found SEQXE filtered fastq for ${filename}"
+echo "#"
+
+else
+
+echo "#scRepli-seq Step1 SEQXE Trimming     start: $(date)"
 min_length=30
 cutadapt -b ${SEQXE} -e 0.09 -O 19 -m ${min_length} -o ${out_fastq2} ${out_fastq1}
+echo "#scRepli-seq Step1 Whole trimming steps end: $(date)"
 
 fi
 
-echo "#scRepli-seq Step1 Whole trimming steps	end: `date`"
+fi
